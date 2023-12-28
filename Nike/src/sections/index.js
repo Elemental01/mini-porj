ff1a003917253c4d898df5bf1b443962cc164bf0
Elemental01 <raiharsh022@gmail.com>
2023-12-28 21:37:01 +0530

    Inital commit

---
+++ Nike/src/sections/index.js
@@ -0,0 +1,18 @@
+import Hero from "./Hero";
+import CustomerReviews from "./CustomerReviews";
+import Footer from "./Footer";
+import PopularProducts from "./PopularProducts";
+import SuperQuality from "./SuperQuality";
+import Subscribe from "./Subscribe";
+import SpecialOffer from "./SpecialOffer";
+import Services from "./Services";
+export {
+  Hero,
+  CustomerReviews,
+  Footer,
+  PopularProducts,
+  SpecialOffer,
+  Subscribe,
+  SuperQuality,
+  Services,
+};
